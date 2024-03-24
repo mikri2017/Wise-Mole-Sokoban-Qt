746f8680abae750a22f6880aa730682e215ec1cb
MIKRI <2007mik007@mail.ru>
2024-03-24 21:17:16 +0700

    Прикинул структуру БД, написал процедуры для работы с пользователями - авторами

---
+++ WiseMoleServer/db_structure/wise_mole_qt.sql
@@ -0,0 +1,197 @@
+-- MariaDB dump 10.19-11.2.3-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: wise_mole_qt
+-- ------------------------------------------------------
+-- Server version	11.2.3-MariaDB-1:11.2.3+maria~deb12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `wise_mole_qt`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `wise_mole_qt` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `wise_mole_qt`;
+
+--
+-- Table structure for table `levels`
+--
+
+DROP TABLE IF EXISTS `levels`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `levels` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID уровня',
+  `user_id` int(10) unsigned NOT NULL COMMENT 'ID пользователя - автора уровня',
+  `name` varchar(100) NOT NULL COMMENT 'Наименование уровня',
+  `data` varchar(500) NOT NULL COMMENT 'Содержимое (карта) уровня',
+  PRIMARY KEY (`id`),
+  KEY `levels_users_FK` (`user_id`),
+  CONSTRAINT `levels_users_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID пользователя - автора уровней',
+  `name` varchar(50) NOT NULL COMMENT 'Логин пользователя',
+  `pass_hash` varchar(100) NOT NULL COMMENT 'Хэш пароля пользователя',
+  `token` varchar(100) DEFAULT NULL COMMENT 'Токен авторизации',
+  `token_end_time` datetime DEFAULT (current_timestamp() + interval 4 hour) COMMENT 'Дата завершения актуальности токена',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_unique` (`name`),
+  UNIQUE KEY `users_token_IDX` (`token`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Пользователи - авторы уровней';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'wise_mole_qt'
+--
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP FUNCTION IF EXISTS `user_add` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `user_add`(in login varchar(50), in pass_hash varchar(100)
+) RETURNS int(11)
+BEGIN
+	declare cnt int;
+
+	-- Проверим наличие такого пользователя
+	select
+		count(1)
+	from users u
+	where u.name = login
+	into cnt;
+
+	if cnt > 0 then
+		-- Пользователь с таким логином уже есть
+		return 1;
+	end if;
+
+	insert into users (
+		name,
+		pass_hash
+	) values (
+		login,
+		pass_hash
+	);
+
+	return cnt;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP FUNCTION IF EXISTS `user_auth` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `user_auth`(in login varchar(50), in pass_hash varchar(100), in token varchar(100)
+) RETURNS int(11)
+BEGIN
+    declare user_id int unsigned;
+
+    select
+        u.id
+    from users u
+    where u.name = login
+        and u.pass_hash = pass_hash
+    into user_id;
+
+    if user_id is null then
+        -- Авторизация провалена
+        return 1;
+    end if;
+
+    update users u
+    set u.token = token,
+        u.token_end_time = date_add(now(), interval 4 hour)
+    where u.id = user_id;
+
+    return 0;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP FUNCTION IF EXISTS `user_chg_pass` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `user_chg_pass`(in token varchar(100), in pass_hash_old varchar(100), in pass_hash_new varchar(100)
+) RETURNS int(11)
+BEGIN
+    declare user_id int unsigned;
+
+    select
+        u.id
+    from users u
+    where u.token = token
+        and u.token_end_time > now()
+        and u.pass_hash = pass_hash_old
+    into
+        user_id;
+    
+    if user_id is null then
+        -- Авторизация устарела, либо нет такого пользователя
+        return 1;
+    end if;
+
+    update users u
+    set u.pass_hash = pass_hash_new
+    where u.id = user_id;
+
+    return 0;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-24 21:14:06
